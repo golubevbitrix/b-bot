eb83dd7f1b0b49e14b0903e339d8c22c78f9e2f2
golubevbitrix <golubevbitrix@gmail.com>
2025-06-11 17:18:52 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -20,7 +20,7 @@ def chat_code(request):
 
 async def chat_id(code):
   async with httpx.AsyncClient() as client:
-    data = {"USER_CODE": code}
+    data = {"USER_CODE": f'"{code}"'}
     response = await client.post('https://b24-dqlsji.bitrix24.ru/rest/1/s8xdt6lup9f63cj2/imopenlines.session.open', data=data)
     response = response.json()
     return str(response["result"]["chatId"])
