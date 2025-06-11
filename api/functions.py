33c698647741cb239c3aabe4a22df63c5bca648e
golubevbitrix <golubevbitrix@gmail.com>
2025-06-11 15:42:25 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -1,6 +1,11 @@
 from urllib.parse import unquote
 import httpx
 import re
+import asyncio
+import asyncpg
+import time
+
+connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 
 def chat_code(request):
   data = {}
@@ -16,9 +21,24 @@ async def chat_id(code):
   async with httpx.AsyncClient() as client:
     data = {"USER_CODE": code}
     response = await client.post('https://b24-dqlsji.bitrix24.ru/rest/1/s8xdt6lup9f63cj2/imopenlines.session.open', data=data)
-    return response.json()
+    response = response.json()
+    return response["result"]["chatId"]
     
-  
+async def record_time(chat):
+    pool = await asyncpg.create_pool(connection_string)
+    timestamp = str(time.time())
+    statement = f"""
+      INSERT INTO chats (id, time)
+      VALUES ('{chat}', '{timestamp}')
+      ON CONFLICT (id)
+      DO NOTHING | DO UPDATE SET id = '{chat}', time = '{timestamp}';
+    """
+    async with pool.acquire() as conn:
+    # Execute a statement to create a new table.
+        await conn.execute(statement)
+    await pool.close()  
+
+async def update_message_time(
 #def find(array, term):
   #for i in array:
     #if 
