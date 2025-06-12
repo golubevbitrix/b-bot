f2d2362a7c9045b14be0f6cced2c071ebc895b5b
golubevbitrix <golubevbitrix@gmail.com>
2025-06-12 12:52:03 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
-from api.functions import handle_new_message
+from api.functions import handler
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
@@ -20,7 +20,7 @@ async def send_message(request: Request):
     print(request, unquote(body.decode()))
     print(urlparse(body.decode()))
     try:
-        await handle_new_message(body.decode())
+        await handler(body.decode())
     except Exception as e:
         print(e)
 
