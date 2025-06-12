50859744e62cde2d4c7fcd6cf4a0bb20091d1535
golubevbitrix <golubevbitrix@gmail.com>
2025-06-12 13:46:23 +0500

    Update check.py

--- api/check.py
+++ api/check.py
@@ -10,11 +10,13 @@ connection_string = 'postgresql://neondb_owner:npg_ZEKV2AOWjyp9@ep-raspy-rice-a2
 async def update_chat(chat):
     pool = await asyncpg.create_pool(connection_string)
     timestamp = time.time()
+    lines = await get_lines()
     async with pool.acquire() as conn:
     # Execute a statement to create a new table.
         data = await conn.execute(statement)
         data = [dict(row) for row in data]
-        
+        for row in data:
+            
     await pool.close()  
 
 async def change_user(chat, user):
@@ -22,13 +24,15 @@ async def change_user(chat, user):
       data = {"CHAT_ID": chat, "TRANSFER_ID": user}
       response = await client.post('https://bitrix.abramovteam.ru/rest/1/0bwuq2j93zpaxkie/imopenlines.operator.transfer', data=data)
       response = response.json()
-async def get_users_on_lines():
+        
+async def get_lines():
     async with httpx.AsyncClient() as client:
-      
       lines = []
       response = await client.post('https://bitrix.abramovteam.ru/rest/1/0bwuq2j93zpaxkie/imopenlines.config.list.get')
-      response = response.json()
-      for line in response["result"]:
+      json = response.json()
+      for line in json["result"]:
           data = {"CONFIG_ID": line["id"]}
-          
-      return response["result"]["QUEUE"]
+          response = await client.post('https://bitrix.abramovteam.ru/rest/1/0bwuq2j93zpaxkie/imopenlines.config.get', data=data)
+          result = response.json()["result"]
+          lines.append({"id":result["id"], "queue": result["QUEUE"]})
+      return lines
