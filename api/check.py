03e071363b2ff574e3a3242c63679dbc74b8d350
golubevbitrix <golubevbitrix@gmail.com>
2025-06-13 23:51:21 +0500

    Update check.py

--- api/check.py
+++ api/check.py
@@ -27,7 +27,7 @@ async def update_handler():
         for row in data:
             print(row)
             print(timestamp - int(row["time"]))
-            if timestamp - int(row["time"]) > 240 && row["active"] == 'Y':
+            if timestamp - int(row["time"]) > 240 and row["active"] == 'Y':
                 print('queue: ', lines[row["line"]])
                 user = row["user_id"]
                 queue = lines[row["line"]]
