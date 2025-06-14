f4194e0e7e7bff5b242b6aa86506c3a31de66c72
golubevbitrix <golubevbitrix@gmail.com>
2025-06-14 08:18:59 +0500

    Update check.py

--- api/check.py
+++ api/check.py
@@ -9,6 +9,7 @@ import asyncpg
 import time
 #import aioredis
 
+load_dotenv(dotenv_path=".env")
 api = os.getenv("api")
 connection_string = os.getenv("postgresql")
 #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
