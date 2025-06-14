eb870a1f31036c8a99eceb5056ed294afd3a3dfd
golubevbitrix <golubevbitrix@gmail.com>
2025-06-14 09:15:43 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
 from api.functions import hook_handler
 from api.check import update_handler
-from api.update-redis import redis_update_handler
+from api.update_redis import redis_update_handler
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
