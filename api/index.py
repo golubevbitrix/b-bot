b38426d2f7ed5a9c70b041f74bd3e24d18c12e02
golubevbitrix <golubevbitrix@gmail.com>
2025-07-12 20:55:12 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -26,12 +26,6 @@ async def send_message(request: Request):
     except Exception as e:
         print(e)
 
-    #code = chat_code(body.decode())
-    #id = await chat_id(code)
-    #print(id)
-    #update_dict = await request.json()
-    #print(update_dict)
-    #await tgbot.send_message('A message sent')
     return "post accepted"
 
 @app.get('/api/message')
@@ -51,11 +45,8 @@ async def update(request: Request):
     try:
         await redis_update_handler()
     except Exception as e:
-        print(e)
-        #exc_type, exc_value, exc_traceback = sys.exc_info()
-        #tb_list = traceback.extract_tb(exc_traceback)
-        #line_number = tb_list[-1].lineno  # Get the line number of the error
-        #print(f"Exception occurred at line: {line_number}")
+        print("Exception: ", e)
+
 @app.get('/api/chat')
 async def update(request: Request, chat: str):
     try:
