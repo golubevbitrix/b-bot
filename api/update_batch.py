04a7727f7b61127ee8d99ace9eb00df1a26dd6f8
golubevbitrix <golubevbitrix@gmail.com>
2025-07-18 00:52:54 +0500

    Update update_batch.py

--- api/update_batch.py
+++ api/update_batch.py
@@ -140,7 +140,8 @@ async def handle_unsorted():
             printn(key, "completed")
         except Exception as e:
             printn(f"{unsorted[key]} has not been deleted for {e}")
-            
+    printn("sorting finished ")
+    
 async def get_data(chats):
     result = await batch_request('imopenlines.dialog.get', 'CHAT_ID', chats)
     return result
