efade39caee2668f349b953a9ecd4209e01a0685
golubevbitrix <golubevbitrix@gmail.com>
2025-07-17 17:43:19 +0500

    Update update_batch.py

--- api/update_batch.py
+++ api/update_batch.py
@@ -256,7 +256,7 @@ async def get_chat_history(chat, session):
     async with httpx.AsyncClient() as client:
         async with client.stream('GET', url) as response:
             count = 0
-            async for chunk in response.aiter_text(10):
+            async for chunk in response.aiter_text(2000):
                 print(count, chunk)
                 count += 1
 
